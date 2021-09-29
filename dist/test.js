806ba300094b2595f58cbbc119665f6c308b057e
Luca Saccone <lu.saccone@gmail.com>
2021-09-29 15:46:14 +0200

    add Factory Design Pattern

--- dist/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function foo(bar) {
-    return 'Hello, ' + bar;
-}
-console.log(foo('ABC'));
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-function foo(bar:string){
-    return 'Hello, ' + bar;
-}
-
-console.log(foo('ABC'))
